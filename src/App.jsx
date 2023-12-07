ba703d4de38238a34073b05d7fd8bab4f9ccb948
Juan Carlos Ruiz Gomez <18002171@alumnos.utleon.edu.mx>
2023-12-07 11:36:40 -0600

    agregacion de login, carrito y pedidos al react

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Carrito from "./components/Carrito";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Pago from "./components/Pago";
+import Pedidos from "./components/Pedidos";
+import Login from "./components/Login";
+
+const stripePromise = loadStripe(
+  "pk_test_51MtEODBMu9RgSpPE73uA5jOG7SYLaorW6hU8u4wE1VNN6ERcPu19SiwPtusKsEXzQTYD6IdcOYgx5c1XlkvEufDg00EH0GUDX4"
+  //'sk_test_51MtEODBMu9RgSpPEini9G9YdSjjoepnch1SljAmu7plwuVrwEm8QhxNs63Pi7585IZo7byXP2ee64jHukEobNc7c00LUPvOz6q'
+);
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="login" element={<h1>login</h1>} />
+        <Route path="login" element={<Login />} />
         <Route path="registrar" element={<h1>registrar</h1>} />
         <Route path="inventario" element={<h1>inventario</h1>} />
         <Route path="productos" element={<h1>productos</h1>} />
         <Route path="recetas" element={<h1>recetas</h1>} />
         <Route path="materiaPrima" element={<h1>materiaPrima</h1>} />
         <Route path="compras" element={<h1>compras</h1>} />
-        <Routes path="ventas">
-          <Route path="ventas/carrito" element={<h1>ventas</h1>} />
-          <Route path="ventas/pago" element={<h1>ventas</h1>} />
-        </Routes>
+        <Route path="ventas/carrito" element={<Carrito />} />
+        <Route
+          path="ventas/pago"
+          element={
+            <Elements stripe={stripePromise}>
+              <Pago />
+            </Elements>
+          }
+        />
         <Route path="proveedores" element={<h1>proveedores</h1>} />
         <Route path="clientes" element={<h1>clientes</h1>} />
         <Route path="usuarios" element={<h1>usuarios</h1>} />
-        <Route path="pedidos" element={<h1>pedidos</h1>} />
+        <Route path="pedidos" element={<Pedidos />} />
         <Route path="finanzas" element={<h1>facturas</h1>} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
---
+++ src/components/Pago.jsx
@@ -0,0 +1,193 @@
+import React, { useEffect, useState } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import axios from "axios";
+
+axios.defaults.headers.common["Accept"] = "application/json";
+
+const Pago = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [productosEnCarrito, setProductosEnCarrito] = useState([]);
+  const [direccion, setDireccion] = useState("");
+
+  // Cargar los datos del carrito al inicio (si existen en localStorage)
+  useEffect(() => {
+    const carritoGuardado = localStorage.getItem("productosEnCarrito");
+    if (carritoGuardado) {
+      setProductosEnCarrito(JSON.parse(carritoGuardado));
+    }
+  }, []);
+
+  const calcularTotalAPagar = () => {
+    return productosEnCarrito.reduce(
+      (total, producto) => total + producto.precio_venta * producto.cantidad,
+      0
+    );
+  };
+
+  const redirigirPedidos = () => {
+    // Aquí puedes poner el enlace de la pasarela de pago
+    const enlacePedidos = "http://localhost:5173/pedidos";
+    window.location.href = enlacePedidos;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const result = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+
+    if (result.error) {
+      console.error(result.error.message);
+    } else {
+      const fechaActual = new Date(); // Obtener la fecha actual
+      fechaActual.setHours(fechaActual.getHours() - 6); // Restar 6 horas a la fecha actual
+      const fecha_hora_pedido = fechaActual.toISOString(); // Formatear la fecha a ISO string
+      fechaActual.setDate(fechaActual.getDate() + 3); // Sumar 3 días a la fecha actual
+      const fecha_hora_entrega = fechaActual.toISOString(); // Formatear la fecha a ISO string
+      // Luego para obtener la data
+      const user = JSON.parse(localStorage.getItem("usuario"));
+      // Acceder al ID
+      const userId = user.id;
+
+      // Estructura de datos para la inserción del pedido
+      const datosPedido = {
+        id_usuario: userId,
+        estado_pedido: 1,
+        fecha_hora_pedido: fecha_hora_pedido,
+        domicilio: direccion,
+        empleado: 1,
+        fecha_hora_entrega: fecha_hora_entrega,
+      };
+
+      // Realizar la inserción en el servidor
+      axios
+        .post("https://localhost:7075/api/Pedidos", datosPedido)
+        .then((response) => {
+          // Estructura de datos para la inserción de los pedidosProdructos
+          for (let index = 0; index < productosEnCarrito.length; index++) {
+            const producto = productosEnCarrito[index];
+            const datosPedido1 = {
+              id: 0,
+              id_pedido: response.data.id_pedido, // Reemplaza con el ID del pedido
+              id_producto: producto.id_producto,
+              cantidad: producto.cantidad,
+            };
+            // Realizar la inserción en el servidor
+            axios
+              .post("https://localhost:7075/api/PedidosProductos", datosPedido1)
+              .then((response) => {
+                // Estructura de datos para la inserción de los pedidosProdructos
+                for (
+                  let index = 0;
+                  index < productosEnCarrito.length;
+                  index++
+                ) {
+                  const producto = productosEnCarrito[index];
+                  const datosProducto = {
+                    nombre: "",
+                    descripcion: "",
+                    cantidad_disponible:
+                      producto.cantidad_disponible - producto.cantidad,
+                  };
+                  // Realizar la inserción en el servidor
+                  axios
+                    .put(
+                      `https://localhost:7075/api/Productos/` +
+                        producto.id_producto,
+                      datosProducto
+                    )
+                    .then((response) => {
+                      console.log(
+                        "Producto actualizado exitosamente:",
+                        response.data
+                      );
+                      // Realizar acciones adicionales si es necesario
+                    })
+                    .catch((error) => {
+                      console.error("Error al insertar datos:", error);
+                    });
+                }
+                redirigirPedidos();
+              })
+              .catch((error) => {
+                console.error("Error al insertar datos:", error);
+              });
+          }
+        })
+        .catch((error) => {
+          console.error("Error al insertar datos:", error);
+        });
+    }
+  };
+
+  const cardStyle = {
+    backgroundColor: "#f9f9f9", // Color hueso
+    padding: "20px", // Añade padding al contenido de la tarjeta
+    boxShadow: "0 4px 8px rgba(0,0,0,0.1)", // Añade sombra a la tarjeta
+    borderRadius: "8px", // Agrega bordes redondeados
+  };
+
+  return (
+    <div className="container mt-4">
+      <h1 style={{ textAlign: "center" }}>Pasarela de Pago</h1>
+      <form onSubmit={handleSubmit} className="my-4">
+        <div className="card p-4" style={cardStyle}>
+          <p className="mb-3">
+            Total a pagar: $
+            {productosEnCarrito.length > 0 ? calcularTotalAPagar() : 0}
+          </p>
+          <div className="mb-3">
+            <label htmlFor="direccion" className="form-label">
+              Dirección de envío:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="direccion"
+              placeholder="Ingrese su dirección de envío"
+              value={direccion}
+              onChange={(e) => setDireccion(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Detalles de tarjeta:</label>
+            <CardElement
+              className="form-control"
+              options={{
+                style: {
+                  base: {
+                    fontSize: "16px",
+                    color: "#424770",
+                    "::placeholder": {
+                      color: "#aab7c4",
+                    },
+                  },
+                  invalid: {
+                    color: "#9e2146",
+                  },
+                },
+              }}
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={!stripe}
+            className="btn btn-success btn-lg d-block w-100"
+          >
+            Realizar Compra
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Pago;
